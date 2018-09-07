4f5fc7634400ad2753d5f181b84482f7cec926df
Johnathan Barrett <johnathan.barrett@avbrowne.com>
2018-09-07 10:08:03 +0100

    Changed how table data is stored, we don't really care what's a title and what's data in the table view. Cells navigatable by arrow keys so it's basically hitting feature parity with excel now.

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="data-table" @key.up="previousRow">
+
         <table v-click-outside="clickOut">
             <tr v-for="(row, rowIndex) in internalRows">
                 <td v-for="(cellValue, cellIndex) in row">
@@ -7,6 +8,7 @@
                             type="text"
                             v-model="row[cellIndex]"
                             :class="cellClass(rowIndex, cellIndex)"
+                            @click.right="testRightClick"
                             @mousedown.prevent="gotoCell({rowIndex, cellIndex})"
                             @keydown.up.prevent="gotoCell({rowIndex: rowIndex - 1, cellIndex})"
                             @keydown.down.prevent="gotoCell({rowIndex: rowIndex + 1, cellIndex})"
@@ -15,74 +17,83 @@
                             :ref="`cell-${rowIndex},${cellIndex}`"
                     >
                 </td>
+                <td @click="addColumn" v-if="rowIndex === 0">Add Column</td>
+            </tr>
+            <tr>
+                <td @click="addRow">Add Row</td>
             </tr>
         </table>
-        <button @click.prevent="addRow">Add Row</button>
-        <button @click.prevent="addColumn">Add Column</button>
     </div>
 </template>
 
 <script>
-export default {
-  props: {
-    rows: {
-      type: Array,
-      default() {
-        return [
-          [null, 'Column 1', 'Column 2'],
-          ['TITLE', 'Cell 1,1', 'Cell 1,2'],
-          ['TITLE2', 'Cell 2,1', 'Cell 2,2'],
-        ];
-      },
-    },
-  },
-  data() {
-    return {
-      activeRowIndex: null,
-      activeCellIndex: null,
-      internalRows: [],
+    export default {
+        props: {
+            rows: {
+                type: Array,
+                default() {
+                    return [
+                        [null, 'Column 1', 'Column 2'],
+                        ['TITLE', 'Cell 1,1', 'Cell 1,2'],
+                        ['TITLE2', 'Cell 2,1', 'Cell 2,2'],
+                    ];
+                },
+            },
+        },
+        data() {
+            return {
+                contextMenuVisible: true,
+                contextMenuPosition: [0,0],
+                activeRowIndex: null,
+                activeCellIndex: null,
+                internalRows: [],
+            };
+        },
+
+        mounted() {
+            this.internalRows = this.rows;
+        },
+
+        methods: {
+            testRightClick() {
+              console.log('right?');
+            },
+            gotoCell(cell) {
+                const {rowIndex, cellIndex} = cell;
+
+                if(typeof this.rows[rowIndex] !== 'undefined' && typeof this.rows[rowIndex][cellIndex] !== 'undefined') {
+                    this.activeRowIndex = rowIndex;
+                    this.activeCellIndex = cellIndex;
+
+                    this.$refs[`cell-${rowIndex},${cellIndex}`][0].select();
+                }
+            },
+
+            clickOut() {
+                this.activeRowIndex = null;
+                this.activeCellIndex = null;
+            },
+
+            cellClass(rowIndex, cellIndex) {
+                return {
+                    'active-cell': (this.activeRowIndex === rowIndex && this.activeCellIndex === cellIndex),
+                };
+            },
+
+            addRow() {
+                this.internalRows.push(this.internalRows[0].map(() => null));
+                this.gotoCell({rowIndex: this.activeRowIndex, cellIndex: this.activeCellIndex});
+            },
+
+            addColumn() {
+                this.internalRows.map((row) => {
+                    row.push(null);
+                    return row;
+                });
+                this.gotoCell({rowIndex: this.activeRowIndex, cellIndex: this.activeCellIndex});
+            },
+        },
     };
-  },
-
-  mounted() {
-    this.internalRows = this.rows;
-  },
-
-  methods: {
-    gotoCell(cell) {
-      const { rowIndex, cellIndex } = cell;
-
-      if (typeof this.rows[rowIndex] !== 'undefined' && typeof this.rows[rowIndex][cellIndex] !== 'undefined') {
-        this.activeRowIndex = rowIndex;
-        this.activeCellIndex = cellIndex;
-
-        this.$refs[`cell-${rowIndex},${cellIndex}`][0].select();
-      }
-    },
-
-    clickOut() {
-      this.activeRowIndex = null;
-      this.activeCellIndex = null;
-    },
-
-    cellClass(rowIndex, cellIndex) {
-      return {
-        'active-cell': (this.activeRowIndex === rowIndex && this.activeCellIndex === cellIndex),
-      };
-    },
-
-    addRow() {
-      this.internalRows.push(this.internalRows[0].map(() => null));
-    },
-
-    addColumn() {
-      this.internalRows.map((row) => {
-        row.push(null);
-        return row;
-      });
-    },
-  },
-};
 </script>
 
 <style scoped lang="scss">
